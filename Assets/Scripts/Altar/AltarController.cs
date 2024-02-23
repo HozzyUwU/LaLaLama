d78362bf812eaaea9e54fcef8f2941f120898b83
Kirill "Hozzy" Vasilev <crashezmeister@gmail.com>
2024-02-23 23:48:01 +0300

    FuzzyLogic was implemented

--- Assets/Scripts/Altar/AltarController.cs
+++ Assets/Scripts/Altar/AltarController.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
+[RequireComponent(typeof(IFuzzyProvider))]
 public class AltarController : MonoBehaviour, IInteractable
 {
+    public static event Action<List<string>> OnAltarUpdate;
+    public static event Action<List<string>> OnAltarLose;
+    public static event Action<List<string>> OnAltarWin;
+
+    public List<string> Items => _items;
+
     [Header("References")]
     [Space]
     [SerializeField] private InventoryManager _inventory;
+    private IFuzzyProvider _fuzzyProvider;
 
     [Header("Holder")]
     [Space]
-    [SerializeField] private string _item;
+    [SerializeField] private List<string> _items = new List<string>();
+
+    private void Awake() 
+    {
+        if(!TryGetComponent<IFuzzyProvider>(out _fuzzyProvider))
+        {
+            Debug.LogWarning("There Is No IFuzzyProvider");
+        }    
+    }
 
     public void Interact()
     {
@@ -18,7 +36,28 @@ public class AltarController : MonoBehaviour, IInteractable
 
         if(item != "")
         {
-            _item = item;
+            _items.Add(item);
+            Debug.Log($"Fuzzy score: {_fuzzyProvider.ProvideFuzzyCalculation(_items)}");
+
+            if(_items.Count == 3)
+            {
+                FuzzyLogic();
+            }
+
+            OnAltarUpdate?.Invoke(_items);
         }
+    }
+
+    private void FuzzyLogic()
+    {
+        if(_fuzzyProvider.ProvideFuzzyCalculation(_items) < 100.0f)
+        {
+            OnAltarLose?.Invoke(_items);
+        }else
+        {
+            OnAltarWin?.Invoke(_items);
+        }
+
+        _items.Clear();
     }
 }
